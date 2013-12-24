f01fedebe2521e38e82d909333af5779c1e8c37b
soidid <soidid@gmail.com>
2013-12-24 22:42:17 +0800

    add files

---
+++ blacklist/pages/index.js
@@ -0,0 +1,25 @@
+
+$(document)
+  .ready(function() {
+
+    $('.filter.menu .item')
+      .tab()
+    ;
+    
+    $('.ui.accordion')
+      .accordion()
+    ;
+    
+    $('.ui.checkbox')
+      .checkbox()
+    ;
+    
+    $('.ui.dropdown')
+      .dropdown()
+    ;
+ 
+    
+  })
+;
+
+
